f0887ee0607a432a6776a5235d12460da1c1e58e
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-13 07:50:50 -0400

    res.end(results);

--- pages/api/lightDataFetch.js
+++ pages/api/lightDataFetch.js
@@ -1,4 +1,3 @@
-// pages/api/hello.js
 export const config = {
     //runtime: 'edge',
     // regions: 'sfo1', // only execute this function on sfo1
@@ -11,7 +10,6 @@ const pool = new Pool({
   database: 'postgres',
   password: 'nextjsvercelswany',
   port: 5433,
-  // client_encoding: 'utf8',
 });
 
 // const Pool = require('pg').Pool
@@ -24,8 +22,6 @@ const pool = new Pool({
 // });
 
 
-// query result -> .rows -> stringify -> results -> res.send(results) -> .json() -> data.results =====>>>> 
-// query result -> stringify -> results -> res.send(results) -> .json() -> data.rows
 export default async function handler(req, res) {
     const db = await pool.connect()
     try {        
@@ -38,57 +34,10 @@ export default async function handler(req, res) {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Cache-Control', 'max-age=180000');
-        res.send(JSON.stringify(result)); // res.send(results); // res.send('pages/index', results); // res.render('pages/index', results );
+        // res.send(JSON.stringify(result)); // res.send(results); // res.send('pages/index', results); // res.render('pages/index', results );
+        res.end(results);
         db.release();
     } catch (error) {
-        // console.error(error);
-        // return new Response(
-        //     JSON.stringify({
-        //         msg: error
-        //     }),
-        //     {
-        //         status: 500,
-        //         headers: {
-        //         'content-type': 'application/json',
-        //         },
-        //     }
-        // )
         console.log( error );
-    } 
-    // finally {
-    //     db.release();
-    // }
+    }
 };
-
-// export default async function handler(req, res) {
-//     try {  
-//         const response = await fetch("https://rickandmortyapi.com/api/character"); // /?page=20
-//         const data = await response.json();
-//         const characters = data.results;
-
-//         return new Response(
-//         JSON.stringify({
-//             characters
-//         }),
-//         {
-//             status: 200,
-//             headers: {
-//             'content-type': 'application/json',
-//             },
-//         }
-//         )
-//     } catch (error) {
-//         console.error(error);
-//         return new Response(
-//         JSON.stringify({
-//             msg: error
-//         }),
-//         {
-//             status: 500,
-//             headers: {
-//             'content-type': 'application/json',
-//             },
-//         }
-//         )
-//     }
-// }
