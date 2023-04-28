1acc8fd6e1963694d96a645b7a691ffba0dca6a4
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-04-28 05:49:25 -0400

    customers?

--- pages/apicall.js
+++ pages/apicall.js
@@ -24,7 +24,7 @@ const ApiCall = () => {
         <h1>LightDataFetch</h1>
         <h2>Customer Data From AWS RDS Postgresql</h2>
         <div>
-        {customers.map((customer) => (
+        {customers?.map((customer) => (
             <div key={customer.customer_id}>
             <h1>Customer ID: {customer.customer_id}</h1>
             <h2>Customer Member Status: {customer.club_member_status}</h2>
